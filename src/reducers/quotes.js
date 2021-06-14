113482e1beb2fbc7e32a0493ddcd08415de2569b
StuartFleishman <Stuart.Alex.Fleishman@gmail.com>
2021-06-14 09:04:05 -0400

    Done.

--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -1,3 +1,36 @@
 export default (state = [], action) => {
-  return state;
+  let newState 
+
+  switch (action.type) {
+    // Do something here based on the different types of actions
+    case 'ADD_QUOTE': 
+      return [...state, action.quote]
+    case 'UPVOTE_QUOTE':
+      newState = state.map(quote => {
+        if (quote.id === action.quoteId) {
+          return { ...quote, votes: quote.votes += 1}
+        }
+        else {
+          return quote;
+        }
+      })
+      return newState; 
+    case 'DOWNVOTE_QUOTE': 
+      newState = state.map(quote => {
+        if(quote.id === action.quoteId && quote.votes > 0) {
+          return {...quote, votes: --quote.votes}
+        }
+        else {
+          return quote
+        }
+      })
+      return newState
+    case 'REMOVE_QUOTE': 
+      return state.filter(quote => quote.id !== action.quoteId)
+    
+    default:
+      // If this reducer doesn't recognize the action type, or doesn't
+      // care about this specific action, return the existing state unchanged
+      return state
+  }
 }
